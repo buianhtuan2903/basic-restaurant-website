27913b70b327ac470da78f31fbadcf9bbe713d6e
Bui Anh Tuan <buianhtuan2903@gmail.com>
2019-12-05 12:54:43 +0700

    update

---
+++ script.js
@@ -0,0 +1,69 @@
+// Fadein
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".fadeout")
+    
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("fadein")} 
+        else {
+            $(tag).removeClass("fadein")}
+    }
+})
+
+// Scrollspy
+$(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 50});   
+  
+    // Add smooth scrolling on all links inside the navbar
+    $(".mouse").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1200, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      }  // End if
+    });
+  });
+
+  $(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 50});   
+  
+    // Add smooth scrolling on all links inside the navbar
+    $(".menu-item a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      }  // End if
+    });
+  });
